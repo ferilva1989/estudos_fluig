8ccfadc0e9bded422c5c0a6369e5e968dea93010
filip <filip@DESKTOP-MERSMSQ>
2022-04-28 22:19:37 -0300

    Exercícios fluig academy

--- forms/Novo Ponto Comercial/cep.js
+++ forms/Novo Ponto Comercial/cep.js
@@ -1,3 +1,8 @@
-/**
- * 
- */
+$("#cep").blur(function() {
+    $.getJSON("//viacep.com.br/ws/" + $("#cep").val() + "/json/", function(dados) {
+    $("#logradouro").val(dados.logradouro);
+    $("#bairro").val(dados.bairro);
+    $("#localidade").val(dados.localidade);
+    $("#estado").val(dados.estado);
+    })
+});
