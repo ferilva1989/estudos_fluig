349a2fa5ba6a3d6741d7198b23c0b6a64a55dd23
ferilva1989 <filipefs.1989@gmail.com>
2022-06-09 22:21:25 -0300

    asdf

---
+++ #fluig Dev Start/JS/objeto.js
@@ -0,0 +1,11 @@
+function externo() {
+    console.log("Externo");
+
+    var pessoa = {
+        Nome : "João",
+        Sobrenome : "Silva",
+        Departamento : "TI",
+    };
+
+    document.getElementById("nome").innerHTML = pessoa.Nome + " trabalha no " + pessoa.Departamento;
+}
